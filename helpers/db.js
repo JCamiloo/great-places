39eb2ee385e3b916efac87bf1c5ee00f1f5484d8
JCamiloo <jcamilo.osorio15@gmail.com>
2020-09-23 23:21:45 -0500

    storing data in the local database

--- helpers/db.js
+++ helpers/db.js
@@ -15,9 +15,21 @@ export const init = () => {
         lng REAL NOT NULL);`,
         [],
         () => resolve(),
-        (_, err) => {
-          reject(err);
-        }
+        (_, err) => reject(err)
+      );
+    });
+  });
+  return promise;
+};
+
+export const insertPlace = (title, imageUri, address, lat, lng) => {
+  const promise = new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?);`,
+        [title, imageUri, address, lat, lng],
+        (_, result) => resolve(result),
+        (_, err) => reject(err)
       );
     });
   });
--- store/actions/places.js
+++ store/actions/places.js
@@ -1,5 +1,6 @@
 import * as FileSystem from "expo-file-system";
 export const ADD_PLACE = "ADD_PLACE";
+import { insertPlace } from "../../helpers/db";
 
 export const addPlace = (title, image) => {
   return async (dispatch) => {
@@ -7,9 +8,21 @@ export const addPlace = (title, image) => {
     const newPath = FileSystem.documentDirectory + fileName;
     try {
       await FileSystem.moveAsync({ from: image, to: newPath });
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        "Dummy address",
+        15.6,
+        12.3
+      );
+
+      console.log(dbResult);
     } catch (err) {
       throw err;
     }
-    dispatch({ type: ADD_PLACE, placeData: { title, image: newPath } });
+    dispatch({
+      type: ADD_PLACE,
+      placeData: { id: dbResult.insertId, title, image: newPath },
+    });
   };
 };
